8801494f35d377fde8edd9d4cd5f2b673ddbde36
Agnet9367 <arsenhavyr@gmail.com>
2025-09-24 16:18:06 +0000

    data type lessons

---
+++ Lesson01_datatypes.py
@@ -0,0 +1,41 @@
+# This is a comment 
+
+# DATA TYPES:
+
+# 1. STRINGS: (normal text)
+print("Hello World")
+print("Hello World, I really love you, you are the t in the world")
+print(" ")
+print("Hello World,\nI really love you,\nyou are the t in the world")
+print("Name: \tArsen \nAge: \t15 \nGrade: \t9")
+
+# 2. INTEGERS: (whole number, negative, 0, positive)
+print(42)
+print(-23)
+print(5 + 5 - 11)
+
+# 3. FLOAT: (decimal)
+print(4.0)
+print(2.0 + 4.5)
+print(2.0 + 4) #returns a float
+
+# 4. BOOLEAN (true, or flase (answer))
+print(True) # 1
+print(False) # 0
+print(True + False)
+print(1 == 2) # Returns False
+print(1 == 1) # Returns True
+print(1 != 1) # Returns False
+print(1 != 2) # Returns True
+print("Hello" == "Hello") # Returns True
+print("Hello" == "hello") # Returns False #It is also case sensative
+print(5 + 5 == 11) # Returns False
+print(5 + 6 == 11) # Returns True
+
+# How do I know what data type I'm working with?
+print("\nDATA TYPE LOOKUP:\n")
+
+print(type("To see this message, wrap me in a print statement"))
+print(type(3))
+print(type(3.14))
+print(type(1 == 1))
