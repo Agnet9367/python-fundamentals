8e5d8a744e8548aa4ce38f15a728049cb709a6a4
Agnet9367 <arsenhavyr@gmail.com>
2025-09-25 17:25:53 +0000

    variables

---
+++ lesson02_variables.py
@@ -0,0 +1,65 @@
+pizza = "Hello, people"
+
+password = "A624h473H532"
+email = "arsenhello@gmail.com"
+print("Password: ", password)
+print("Email: ", email)
+
+temperature = 65.4
+print(type(temperature))
+price = 5.98
+
+enabled = True
+is_complete = True
+print(is_complete)
+print(enabled)
+
+# bad
+x = "A624h473H532"
+y = "arsenhello@gmail.com"
+print("Password: ", x)
+print("Email: ", y)
+
+# for math problems: (good)
+w = 3.14
+e = 10
+print(w + e)
+
+# variables are flexible, you create or update another variable, like so:
+count = 10
+print(count)
+count_down = count - 1
+print (count_down)
+count = count_down
+print (count)
+
+name = "Radia Perlman"
+age = 34
+job = "Networking Engineer"
+
+print(name)
+print(age)
+print(job)
+
+count1 = 10
+print (count1)
+count_up = count1 + 5
+print(count_up)
+
+f = 4
+h = "hello"
+temp = h
+h = f
+f = temp
+print(f)
+print (h)
+
+is_raining = False
+print("is it raining?: ", is_raining)
+is_raining = True
+print("is it raining?: ", is_raining)
+
+
+
+
+
