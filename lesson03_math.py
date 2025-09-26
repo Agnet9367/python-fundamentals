ab9bf720c6c978091f8163ded890f630af6fcbcb
Agnet9367 <arsenhavyr@gmail.com>
2025-09-26 18:00:13 +0000

    bassic math operators

---
+++ lesson03_math.py
@@ -0,0 +1,50 @@
+# KEY CONCEPTS: math operators +, -, *, /, //, %, **
+
+add = 6 + 7
+print("Sum: ", add)
+
+subtract = 6 - 7
+print("Difference: ", subtract)
+
+multiply = 6 * 7
+print("Product: ", multiply)
+
+float_divide2 = 6 / 7
+print("Float Division: ", float_divide2)
+float_divide1 = 8 / 2
+print("Float Division: ", float_divide1)
+
+integer_divide1 = 6 // 7
+print("Integer Division: ", integer_divide1)
+integer_divide2 = 8 // 2
+print("Integer Division: ", integer_divide2)
+
+modulus1 = 7 % 2
+print("Modulus: ", modulus1)
+modulus2 = 6 % 7
+print("Modulus: ", modulus2)
+
+exponent1 = 7 ** 2
+print("Exponent: ", exponent1)
+exponent2 = 6 ** 7
+print("Exponent: ", exponent2)
+
+print(2 + 2 == 10 - 6)
+print(2 + 2 != 10 - 6)
+
+#PEMDAS - order of ops
+
+result1 = (2 + 3) * 4
+print("Result1: ", result1)
+
+result2 = 2 ** 3 * 4
+print("Result2: ", result2)
+
+result3 = 20 / 5 * 2
+print("Result3: ", result3)
+
+result4 = 10 - 2 + 3
+print("Result4: ", result4)
+
+result5 = 5 + 2 ** 3 * (4 - 1)
+print("Result5: ", result5)
