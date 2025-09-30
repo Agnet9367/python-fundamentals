e4292b1ec2c543656e625e74cb4f2ec3170f9355
Agnet9367 <arsenhavyr@gmail.com>
2025-09-30 15:53:49 +0000

    adds math library

---
+++ lesson04_libraries.py
@@ -0,0 +1,11 @@
+# Abstraction hidden code behing 
+import math
+print("\n--- Math Library ---\n")
+
+print("Square root ", math.sqrt(36))
+print("Round up to an integer: ", math.ceil(5.3))
+print("Round down to an integer: ", math.floor(7.7))
+print("Power: ", math.pow(2,6))
+print("Pi:", math.pi)
+
+import random
