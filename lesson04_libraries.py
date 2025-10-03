9ded82e773898024f18567b2e17f7466eaf169d6
Agnet9367 <arsenhavyr@gmail.com>
2025-10-03 02:10:45 +0000

    challanges

--- lesson04_libraries.py
+++ lesson04_libraries.py
@@ -10,14 +10,34 @@ print("Power: ", math.pow(2,6))
 print("Pi:", math.pi)
 
 # Random
+
+seed = 132.4444
+step_1 = seed / 6.7
+step_2 = step_1 - 800
+step_3 = step_2 + 18842
+answer = step_3 
+limitednumber = math.floor(answer * 10)
+print("My Own Random Number Generator:", limitednumber)
+
 import random
-random_integer = random.randint(1, 10)
-print(f"Random integer: {random_integer}")
 
-Seed1 = 3.1497346873247
-Seed1 * 6
-Seed2 = 49756284
-Seed2 - 5
-end = Seed2
-print(end)
+#methods:
+print("Random integer: ", random.randint(1, 10))
+print(random.random())
+print(random.choice(["bread", "chicken", "cheese"]))
+favorites = ["1", "2", "3"]
+print(random.choice(favorites))
+random.shuffle(favorites)
+print(favorites)
 
+#challenge 1
+diameter = 14
+radius = diameter / 2
+Pi = 3.14
+radius2 = radius * radius
+result = math.pi * radius2
+print("Area: ", result)
+
+#challenge 2
+print("Dice: ", random.randint(1, 6))
+ 
