3adc01bd565646b2fc93aa092a10d79bf15a73b8
Agnet9367 <arsenhavyr@gmail.com>
2025-10-16 18:16:32 +0000

    bug

--- lesson08_lists.py
+++ lesson08_lists.py
@@ -35,3 +35,46 @@ print(animals)
 last_animal = animals.pop()
 print("Removed animal: ", last_animal)
 print("Remaining animals: ", animals)
+
+animal_to_replace = animals.index("monkey")
+print(animal_to_replace)
+
+empty_list = []
+empty_list.append("Thing")
+print(empty_list)
+
+nums = [3,7,4,2,6,9,1,5,8,0]
+print("Length of the list: ", len(nums))
+print("Min: ", min(nums))
+print("Max: ", max(nums))
+print("Sum: ", sum(nums))
+
+nums.sort()
+print(nums)
+animals.sort()
+print(animals)
+
+if "cat" in animals:
+    print("Cat is in the list!")
+else:
+    print("Cat is not in the list!")
+
+original_list = [1, 2, 3]
+copied_list = original_list.copy()
+copied_list.append(4)
+print(original_list)
+print(copied_list)
+
+matrix = [ 
+    [1,2,3], 
+    [4,5,6], 
+    [7,8,9] 
+    ]
+ 
+print(matrix[2])
+print(matrix[0][2])
+
+#Challange 1
+list2 = [1, 2, 3, 4, 5, 6]
+ulist = int(input("Enter a new integer: "))
+
