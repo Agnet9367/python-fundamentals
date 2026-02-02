3b04bcca588b55ef1e0e0076e030783850ff5ba5
Agnet9367 <arsenhavyr@gmail.com>
2026-02-02 18:43:50 +0000

    commit

---
+++ lesson10_functions.py
@@ -0,0 +1,15 @@
+# Python functions are simply blocks of code that can be reused
+# To run a function, you "call" the function by writing its name, followed by parenthesis, and any arguments that it needs.
+
+print("Functions (Procedures)")
+
+def say_hi():
+    print("Hi!")
+def say_bye():
+    print("Bye!")
+
+
+say_hi()
+say_bye()
+say_hi()
+say_bye()
